e93039556be79ecad59066d5f4111158f90aa02c
danyosa <danyosa@csticorp.biz>
2019-08-13 00:20:31 -0500

    Fin de la sección 10

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-//Inicializando varibles
+//Inicializando variables
 var app= express();
+
+//BodyParser
+app.use(bodyParser.urlencoded({ extented: false}));
+app.use(bodyParser.json());
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var loginRoutes = require('./routes/login');
+var appUsuario = require('./routes/usuario');
+
 
 //Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true },
@@ -12,13 +23,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlP
         console.log('Conexión correcta a la base de datos: \x1b[32m%s\x1b[0m', 'OK');
     });
 
-// Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+//Rutas
+app.use('/usuario',appUsuario);
+app.use('/login',loginRoutes);
+app.use('/',appRoutes);
+
 //Escuchar peticiones
 app.listen(3000, () =>{
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
